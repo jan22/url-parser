73a96f16461dafa9dcacaf674849ecb13e43cc74
Jan Niebuhr <j.niebuhr@haven5.de>
2016-01-14 13:59:54 +0100

    refactoring tests

--- src/URLParserTest.java
+++ src/URLParserTest.java
@@ -8,48 +8,49 @@ import static org.junit.Assert.*;
 
 public class URLParserTest {
 
+    private URLParser p = new URLParser();
+
     @Test
     public void shouldBeAHttpProtocol() {
-        URLParser p = new URLParser("http://www.jan-niebuhr.com");
+        p.setUrl("http://www.jan-niebuhr.com");
         assertTrue("Result should be http!", p.getProtocol().equals("http"));
     }
 
     @Test
     public void shouldNotBeAHttpProtocol() {
-        URLParser p = new URLParser("https://www.jan-niebuhr.com");
+        p.setUrl("https://www.jan-niebuhr.com");
         assertFalse("Result should not be http!", p.getProtocol().equals("http"));
     }
 
     @Test
     public void shouldBeAFtpProtocol () {
-        URLParser p = new URLParser("ftp://ftp.jan-niebuhr.com");
+        p.setUrl("ftp://ftp.jan-niebuhr.com");
         assertTrue("Result should be ftp!", p.getProtocol().equals("ftp"));
     }
 
     @Test
     public void shouldNotHaveAProtocol () {
-        URLParser p = new URLParser("www.jan-niebuhr.com");
+        p.setUrl("www.jan-niebuhr.com");
         assertTrue("Result should not have a Protocol!", p.getProtocol().isEmpty());
     }
 
     @Test
     public void shouldHaveSiteContext () {
-        URLParser p = new URLParser("www.jan-niebuhr.com");
+        p.setUrl("www.jan-niebuhr.com");
         assertTrue("Result should have site context", p.getSite().equals("www.jan-niebuhr.com"));
     }
 
     @Test
     public void shouldHaveAPath() {
-        URLParser p = new URLParser("ftp://ftp.jan-niebuhr.com/yeha/man");
+        p.setUrl("ftp://ftp.jan-niebuhr.com/yeha/man");
         assertTrue("Result should have a path!", p.getPath().equals("yeha/man"));
         assertFalse("Blaaaa", p.getPath().isEmpty());
     }
 
     @Test
     public void shouldNotHaveAPath () {
-        URLParser p = new URLParser("ftp://ftp.jan-niebuhr.com");
+        p.setUrl("ftp://ftp.jan-niebuhr.com");
         assertTrue("Result should not have a path!", p.getPath().isEmpty());
     }
-
 
 }
