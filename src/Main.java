2d2dc3fbbab81b1582dfba2deaddb98d9b2960fb
Jan Niebuhr <j.niebuhr@haven5.de>
2016-01-14 11:42:32 +0100

    add some system outs

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        URLParser p = new URLParser("http://www.jan-niebuhr.com");
+        URLParser p = new URLParser("https://www.jan-niebuhr.com/profile/address");
 
         System.out.println("Protocol: " + p.getProtocol());
+        System.out.println("Site: " + p.getSite());
+        System.out.println("Path: " + p.getPath());
+
     }
 }
